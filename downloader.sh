101233b9b4819d2eb3b2b84fe78d04947c67fc3c
Dani <contact@daniguardiola.me>
2018-11-08 01:54:04 +0100

    Update

--- downloader.sh
+++ downloader.sh
@@ -6,7 +6,7 @@ if [ $# -eq 0 ]
     cd /tmp
     rm -rf .ubuntu-setup ubuntu-setup.zip
     wget -q --no-cache -O ubuntu-setup.zip https://github.com/DaniGuardiola/ubuntu-setup/archive/master.zip
-    unzip ubuntu-setup.zip -d .ubuntu-setup
+    unzip ubuntu-setup.zip -d /tmp
     cd .ubuntu-setup 
     echo "source run.sh 1"
     exit
